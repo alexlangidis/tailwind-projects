dc061ecf4ac6fda14855e278b325ca24a2001830
alexlag7 <alexlag.dev@gmail.com>
2023-09-08 13:59:24 +0300

    main

--- product-card/app/script.js
+++ product-card/app/script.js
@@ -13,15 +13,15 @@ $(document).ready(function () {
             setTimeout(function () {
                 message.html(`You just added ${quantity} it to the cart`).fadeIn(2000, function () {
                     // This function will be called after the fadeIn is complete
-                    message.fadeOut(5000); // Fade out after 2000 milliseconds (2 seconds)
+                    message.fadeOut(5000); // Fade out after 5000 milliseconds (5 seconds)
                 });
-            }, 500); // Wait for 2000 milliseconds (2 seconds)
+            }, 500); // Wait for 500 milliseconds (0.5 seconds)
             
             firstClick = false; // Set the flag to false after the first click
         } else {
             message.html(`You just added ${quantity} to the cart`).fadeIn(1000, function () {
                 // This function will be called after the fadeIn is complete
-                message.fadeOut(4000); // Fade out after 2000 milliseconds (2 seconds)
+                message.fadeOut(4000); // Fade out after 4000 milliseconds (4 seconds)
             });
         }
     });

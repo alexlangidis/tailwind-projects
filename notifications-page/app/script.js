991b754f4f15cc1eec347c4df4f1508dd556e506
alexlag7 <alexlag.dev@gmail.com>
2023-09-09 20:23:48 +0300

    main

---
+++ notifications-page/app/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    
+
+    $('.read-all').click(function (e) { 
+        e.preventDefault();
+
+        let unreadMessages = $('.unread');
+
+        $('.notification').removeClass('unread');
+        $('.notif-dot').addClass('hidden');
+        $('.notif-count').html('0');
+    });
+
+
+    $('.unread').click(function (e) { 
+        e.preventDefault();
+
+        let unreadMessages = $('.unread');
+        
+        if($(this).hasClass('unread')){
+            $(this).removeClass('unread');
+            $(this).find('.notif-dot').addClass('hidden');
+            $('.notif-count').html(unreadMessages.length-1);
+        }
+    });
+
+
+
+});

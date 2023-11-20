6386c3909e0a58339e042e9e45ad6b162d0fa559
alexlag7 <alexlag.dev@gmail.com>
2023-11-20 21:52:19 +0200

    main

---
+++ Discord/src/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.btn-mobile-menu, .menu-close').click(function (e) { 
+        e.preventDefault();
+        $('.asadad').toggleClass('hidden');
+        $('.asd').toggleClass('grid hidden');
+        $('.asd1').toggleClass('hidden');
+
+
+    });
+
+
+});
